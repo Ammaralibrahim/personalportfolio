ee03d34fcb177031a1fc0fb90a4ff4fd414f5d13
ammar_ib <ammaryasir8088@gmail.com>
2023-11-09 23:07:15 +0300

    cdsvs

--- app/page.js
+++ app/page.js
@@ -37,7 +37,16 @@ export default function Home() {
 
   {/* Modal içeriği buraya eklenebilir */}
   <div className="grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-4">
-    <p className="underline text-sky-400">Gamma Cities.</p>
+  <div className="card w-54 bg-base-100 shadow-xl image-full">
+  <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
     <p className="underline text-sky-400">Gamma Studio.</p>
     <p className="underline text-sky-400">Gamma Universe.</p>
     <p className="underline text-sky-400">Reyhana Perfumes.</p>
