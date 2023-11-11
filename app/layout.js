a45aaf3f310753460848e95bd6fe309f78893ab0
ammar_ib <ammaryasir8088@gmail.com>
2023-11-11 15:57:37 +0300

    vdsvdsv

--- app/layout.js
+++ app/layout.js
@@ -5,7 +5,8 @@ const orbitron = Orbitron({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Ammar Alibrahim",
-  description: "“Hello, I'm Ammar Alibrahim, a 16-year-old Full-Stack web developer. I'm passionate about creating innovative and user-friendly web applications using the power of web technologies”",
+  description:
+    "“Hello, I'm Ammar Alibrahim, a 16-year-old Full-Stack web developer. I'm passionate about creating innovative and user-friendly web applications using the power of web technologies”",
 };
 
 export default function RootLayout({ children }) {
@@ -14,15 +15,14 @@ export default function RootLayout({ children }) {
       <body className={orbitron.className}>
         {children}
         <footer className="bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow flex items-center justify-center md:p-6 dark:bg-transparent dark:border-transparent">
-  <span className="text-xs text-gray-500 sm:text-center dark:text-gray-400">
-    © 2023{" "}
-    <a href="/" className="hover:underline">
-      Ammar Alibrahim™
-    </a>
-    . All Rights Reserved.
-  </span>
-</footer>
-
+          <span className="text-xs text-gray-500 sm:text-center dark:text-gray-400">
+            © 2023{" "}
+            <a href="/" className="hover:underline">
+              Ammar Alibrahim™
+            </a>
+            . All Rights Reserved.
+          </span>
+        </footer>
       </body>
     </html>
   );
